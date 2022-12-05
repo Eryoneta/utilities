746a9dfeccaf01740643f9189cfb6da0617b124a
Eryoneta <eryoneta@gmail.com>
2022-12-05 18:23:34 -0300

    List Structure
    - (ALT) 'HasList': Organizado e documentado
    - (ALT) 'SortedList': Documentado

--- utilities/src/tool/list_structure/HashList.java
+++ utilities/src/tool/list_structure/HashList.java
@@ -1,19 +1,55 @@
 package tool.list_structure;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+/** 
+ * Basically, a {@link HashMap} that allows for multiple values, stored inside a {@link List}<br>
+ * The entire set is considered to be a single long list
+ */
 public class HashList<K,V> extends HashMap<K,List<V>>{
-//SERIAL
-	/***/
 	private static final long serialVersionUID = 6830636340927250796L;
 //MAIN
 	public HashList(){}
 //FUNCS
+	
+	/** 
+	 * Adds a value to a sublist under a key
+	 * @param key - The object used to store a value
+	 * @param value - The value to be stored
+	 */
 	public void add(K key,V value){
 		if(!containsKey(key))put(key,new ArrayList<V>());
 		get(key).add(value);
 	}
-//FUNCS: HASH
+	
+	/** 
+	 * Removes a value given an index
+	 * @param index - The index of the value to remove, as counted from the first sublist
+	 * @return The removed value, or null id not found
+	 */
+	public V remove(int index){
+		if(index<0||index>=totalSize())return null;
+		int countedIndex=-1;
+		for(List<V>list:values()){
+			if(countedIndex+list.size()<index) {
+				countedIndex+=list.size();
+				continue;
+			}
+			index-=countedIndex+1;
+			final V value=list.remove(index);
+//			if(list.isEmpty())remove(key);	//É SUBSTITUIDO NO ADD
+			return value;
+		}
+		return null;
+	}
+	
+	/** 
+	 * The index of a given key
+	 * @param key - The key to find the index of
+	 * @return The index of the key, or -1 if not found
+	 */
 	public int indexOfKey(K key){
 		int index=-1;
 		for(K k:keySet()){
@@ -22,54 +58,61 @@ public class HashList<K,V> extends HashMap<K,List<V>>{
 		}
 		return index;
 	}
-//FUNCS: LIST
-@Override
-	public boolean containsValue(Object value){
-		boolean containsValue=false;
-		for(List<V>list:values()){
-			containsValue=list.contains(value);
-			if(containsValue)break;
-		}
-		return containsValue;
-	}
-	public V getValue(int index){
-		if(index<0||index>=totalSize())return null;
-		int countedIndex=0;
-		for(List<V>list:values()){
-			if(countedIndex+list.size()>index){
-				index-=countedIndex;
-				return list.get(index);
-			}else countedIndex=list.size();
-		}
-		return null;
-	}
-	public V remove(int index){
-		if(index<0||index>=totalSize())return null;
-		int countedIndex=0;
-		for(List<V>list:values()){
-			if(countedIndex+list.size()>index){
-				index-=countedIndex;
-				final V value=list.remove(index);
-				if(list.isEmpty())remove(list);
-				return value;
-			}else countedIndex=list.size();
-		}
-		return null;
-	}
-	public int totalSize(){
-		int size=0;
-		for(List<V>list:values())size+=list.size();
-		return size;
-	}
+	
+	/** 
+	 * The index of a given value
+	 * @param value - The value to find the index of
+	 * @return The index, as counted from the first sublist, or -1 if not found
+	 */
 	public int indexOf(V value){
-		int index=-1;
 		int countedIndex=0;
 		for(List<V>list:values()){
 			final int indexOf=list.indexOf(value);
 			if(indexOf!=-1){
-				return countedIndex+index;
-			}else countedIndex=list.size();
+				return countedIndex+indexOf;
+			}else countedIndex+=list.size();
 		}
-		return index;
+		return -1;
+	}
+
+	/** 
+	 * If the value is present in the set
+	 * @param value - The value to check 
+	 * @return True if the value is present, false if not
+	 */
+	@Override public boolean containsValue(Object value){
+		for(List<V>list:values()){
+			if(list.contains(value))return true;
+		}
+		return false;
+	}
+
+	/** 
+	 * Returns a value, given an index
+	 * @param index - The index, as counted from the first sublist
+	 * @return The value if found, null if not
+	 */
+	public V getValue(int index){
+		if(index<0||index>=totalSize())return null;
+		int countedIndex=-1;
+		for(List<V>list:values()){
+			if(countedIndex+list.size()<index) {
+				countedIndex+=list.size();
+				continue;
+			}
+			index-=countedIndex+1;
+			return list.get(index);
+		}
+		return null;
+	}
+	
+	/** 
+	 * The total size of the entire set
+	 * @return The size of all sublists combined
+	 */
+	public int totalSize(){
+		int size=0;
+		for(List<V>list:values())size+=list.size();
+		return size;
 	}
 }
